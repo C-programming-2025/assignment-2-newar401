e95008d4d25d5d319cef55bda86526d891dbdd50
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-12-10 02:02:15 +0545

    Add countdown program in C
    Implement countdown from a user-provided positive integer.

--- src/q17.c
+++ src/q17.c
@@ -1,1 +1,13 @@
-// Write a C program that prompts the user for a positive integer and prints a countdown from that number to 1 using a for loop.
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    scanf("%d", &n);
+
+    for (i = n; i >= 1; i--) {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+    return 0;
+}
