7ef938393497e568f6ec321c8231bf8688f0ba5d
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-12-10 02:00:09 +0545

    Update q4.c

--- src/q4.c
+++ src/q4.c
@@ -1,1 +1,14 @@
-// Write a C program that prints all even numbers between 1 and 100 using a for loop.
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 2; i <= 100; i += 2) {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
